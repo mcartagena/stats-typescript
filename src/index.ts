b9f68ed127172240328222f55afe1a36a3e7134a
Marcelo Cartagena <120571996+marcecarta@users.noreply.github.com>
2022-12-21 16:29:11 -0300

    working with inheritance and composition

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
-import fs from 'fs';
+import { MatchResult } from './MatchResult';
+import { CsvFileReader} from "./CsvFileReader";
+import { MatchReader} from "./MatchReader";
 
-const matches = fs
-  .readFileSync('football.csv', {
-    encoding: 'utf-8',
-  })
-  .split('\n')
-  .map((row: string): string[] => {
-    return row.split(',');
-  });
+// create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
 
-console.log(matches);
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+let manUnitedWins = 0;
+
+for (let match of matchReader.matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
